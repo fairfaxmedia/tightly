5203346b767a68b767269da57906ccdae86fe0ab
Jason Darwin <jcdarwin@gmail.com>
2015-07-21 21:38:58 +1200

    Simply spec

--- spec/sightly-spec.js
+++ spec/sightly-spec.js
@@ -25,66 +25,66 @@ describe("Our template engine", function() {
         expect(tim(mockBefore, data)).toBe(mockAfter);
     });
 
-    it ("should allow interpolation of attribute templates, e.g. '<p data-sly-test=\"${wcmmode.display}\">This is ${msg} stuff</p>'", function() {
+    it ("should allow interpolation of attribute templates, e.g. '<p data-sly-test=\"${wcmmode.display}\">'", function() {
 
         var mockBefore = (function() {/*
-            <p data-sly-test="${wcmmode.display}">This is ${msg} stuff</p>
+            <p data-sly-test="${wcmmode.display}">
         */}).toString().match(/[^]*\/\*([^]*)\*\/\}$/)[1];
 
         var mockAfter = (function() {/*
-            <p data-sly-test="stuff">This is cool stuff</p>
+            <p data-sly-test="stuff">
         */}).toString().match(/[^]*\/\*([^]*)\*\/\}$/)[1];
 
         expect(tim(mockBefore, data)).toBe(mockAfter);
     });
 
-    it ("should allow interpolation of simple 'or' conditional attribute templates, e.g. '<p data-sly-test=\"${wcmmode.edit || wcmmode.display}\">This is ${msg} stuff</p>'", function() {
+    it ("should allow interpolation of simple 'or' conditional attribute templates, e.g. '<p data-sly-test=\"${wcmmode.edit || wcmmode.display}\">'", function() {
 
         var mockBefore = (function() {/*
-            <p data-sly-test="${wcmmode.edit || wcmmode.display}">This is ${msg} stuff</p>
+            <p data-sly-test="${wcmmode.edit || wcmmode.display}">
         */}).toString().match(/[^]*\/\*([^]*)\*\/\}$/)[1];
 
         var mockAfter = (function() {/*
-            <p data-sly-test="stuff">This is cool stuff</p>
+            <p data-sly-test="stuff">
         */}).toString().match(/[^]*\/\*([^]*)\*\/\}$/)[1];
 
         expect(tim(mockBefore, data)).toBe(mockAfter);
     });
 
-    it ("should allow interpolation of simple 'and' conditional attribute templates, e.g. '<p data-sly-test=\"${wcmmode.edit && wcmmode.display}\">This is ${msg} stuff</p>'", function() {
+    it ("should allow interpolation of simple 'and' conditional attribute templates, e.g. '<p data-sly-test=\"${wcmmode.edit && wcmmode.display}\">'", function() {
 
         var mockBefore = (function() {/*
-            <p data-sly-test="${wcmmode.display && wcmmode.edit}">This is ${msg} stuff</p>
+            <p data-sly-test="${wcmmode.display && wcmmode.edit}">
         */}).toString().match(/[^]*\/\*([^]*)\*\/\}$/)[1];
 
         var mockAfter = (function() {/*
-            <p data-sly-test="false">This is cool stuff</p>
+            <p data-sly-test="false">
         */}).toString().match(/[^]*\/\*([^]*)\*\/\}$/)[1];
 
         expect(tim(mockBefore, data)).toBe(mockAfter);
     });
 
-    it ("should allow interpolation of simple '!' conditional attribute templates, e.g. '<p data-sly-test=\"${!wcmmode.edit}\">This is ${msg} stuff</p>'", function() {
+    it ("should allow interpolation of simple '!' conditional attribute templates, e.g. '<p data-sly-test=\"${!wcmmode.edit}\">'", function() {
 
         var mockBefore = (function() {/*
-            <p data-sly-test="${!wcmmode.edit}">This is ${msg} stuff</p>
+            <p data-sly-test="${!wcmmode.edit}">
         */}).toString().match(/[^]*\/\*([^]*)\*\/\}$/)[1];
 
         var mockAfter = (function() {/*
-            <p data-sly-test="true">This is cool stuff</p>
+            <p data-sly-test="true">
         */}).toString().match(/[^]*\/\*([^]*)\*\/\}$/)[1];
 
         expect(tim(mockBefore, data)).toBe(mockAfter);
     });
 
-    it ("should allow interpolation of complex conditional attribute templates, e.g. '<p data-sly-test=\"${wcmmode.edit && wcmmode.display || !wcmmode.edit}\">This is ${msg} stuff</p>'", function() {
+    it ("should allow interpolation of complex conditional attribute templates, e.g. '<p data-sly-test=\"${wcmmode.edit && wcmmode.display || !wcmmode.edit}\">'", function() {
 
         var mockBefore = (function() {/*
-            <p data-sly-test="${wcmmode.edit && wcmmode.display || !wcmmode.edit}">This is ${msg} stuff</p>
+            <p data-sly-test="${wcmmode.edit && wcmmode.display || !wcmmode.edit}">
         */}).toString().match(/[^]*\/\*([^]*)\*\/\}$/)[1];
 
         var mockAfter = (function() {/*
-            <p data-sly-test="true">This is cool stuff</p>
+            <p data-sly-test="true">
         */}).toString().match(/[^]*\/\*([^]*)\*\/\}$/)[1];
 
         expect(tim(mockBefore, data)).toBe(mockAfter);
